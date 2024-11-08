c1de12721bafe850f5367bbde1263c700e3852d3
OregTuz00001 <kanyaimark2007@gmail.com>
2024-11-08 08:58:14 +0100

    fontok

--- Szamologep_projekt.py
+++ Szamologep_projekt.py
@@ -1,3 +1,8 @@
 import tkinter as tk
+import tkinter.font as tkFont
 
 root=tk()
+
+root.title("Számológép")   
+myFont = tkFont.Font(family="Helvetica", size=40, weight="bold")
+btnFont =tkFont.Font(family="Helvetica", size=20)
